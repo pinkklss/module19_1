4f16d228ecbc2a0b1742980032eb408c57ab407c
pinkklss <shaker_04@mail.ru>
2024-11-23 20:48:02 +0700

    Update urls.py

--- store/srorre/urls.py
+++ store/srorre/urls.py
@@ -16,18 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task2 import views as task2_views
-from task4 import views as task4_views
-from task5 import views as task5_views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('class/', task2_views.class_template, name='class_template'),
-    path('function/', task2_views.func_template, name='func_template'),
-    path('platform/', task4_views.main_page, name='platform'),
-    path('games/', task4_views.store_games, name='games'),
-    path('cart/', task4_views.cart_page, name='cart'),
-    path('', task5_views.sign_up_by_html, name='sign_up_by_html'),
-    path('django_sign_up/', task5_views.sign_up_by_django, name='sign_up_by_django'),
 ]
